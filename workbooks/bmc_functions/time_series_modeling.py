7e4d7eed42d29b2066f0fe509fca85f062f59591
Ben McCarty <63123859+BenJMcCarty@users.noreply.github.com>
2021-08-24 23:00:51 -0400

    Most of workflow functionalized

--- workbooks/bmc_functions/time_series_modeling.py
+++ workbooks/bmc_functions/time_series_modeling.py
@@ -6,7 +6,10 @@ By Ben McCarty (bmccarty505@gmail.com)'''
 
 ### --------------- Importing Dependencies --------------- ###
 
-from sys import displayhook
+import matplotlib.pyplot as plt
+import pandas as pd
+import statsmodels.tsa.api as tsa
+
 import pmdarima as pmd
 from pmdarima.arima import ndiffs
 from pmdarima.arima import nsdiffs
@@ -14,12 +17,12 @@ from pmdarima.arima import nsdiffs
 ### --------------- Functions --------------- ###
 
 # Creating train/test split for time series modeling
-def ts_split(timeseries_df, threshold):
+def ts_split(timeseries_df, threshold=.85):
     """Creates train/test split for time series modeling.
 
     Args:
         timeseries_df (DataFrame): DataFrame or Series to be modeled
-        threshold (float): threshold (as decimal percent) for splitting data
+        threshold (float): threshold (as decimal percent) for splitting data. Defaults to .85.
 
     Returns:
         train, test: Initial DataFrame/Series split into sub-Series for modeling
@@ -55,22 +58,45 @@ def model_results(ts_model):
 
 ## Generate best model parameters via auto_arima
 def auto_arima_model(timeseries_dataset,m=12,start_p=0,max_p=3
-                        ,start_q=0,max_q=3,d=n_d,start_P=0,
-                        start_Q=0, max_P=3, max_Q = 3, D = n_D):
+                        ,start_q=0,max_q=3,start_P=0,
+                        start_Q=0, max_P=3, max_Q = 3):
     
+    """Fits an auto_arima model to a given timeseries dataset.
+
+    Args:
+        timeseries_dataset (Series/DataFrame): dataset for modeling
+        m (int, optional): The number of periods in each season (for seasonal differencing). Defaults to 12.
+        start_p (int, optional): Starting value for "p". Defaults to 0.
+        max_p (int, optional): Max value for "p". Defaults to 3.
+        start_q (int, optional): Starting value for "pq. Defaults to 0.
+        max_q (int, optional): Max value for "q". Defaults to 3.
+        start_P (int, optional): Starting value for "P". Defaults to 0.
+        start_Q (int, optional): Starting value for "Q". Defaults to 0.
+        max_P (int, optional): Max value for "P". Defaults to 3.
+        max_Q (int, optional): Max value for "Q". Defaults to 3.
+
+    Returns:
+        auto_arima_model: Fitted auto_arima model for use in SARIMAX modeling.
+    """    
+
+    ## Determine d, D values for SARIMA model
+    n_d = ndiffs(timeseries_dataset)
+    n_D = nsdiffs(timeseries_dataset, m=m)
+
     auto_arima_model = pmd.auto_arima(timeseries_dataset, start_p = start_p,
                                 max_p = max_p,start_q = start_q,
-                                max_q = max_q, d = d ,m = m,
+                                max_q = max_q, d = n_d ,m = m,
                                 start_P = start_P, start_Q = start_Q,
-                                max_P = max_P, max_Q = max_Q, D = D)
+                                max_P = max_P, max_Q = max_Q, D = n_D)
 
     return auto_arima_model
 
 ## Use auto_arima to determine best parameters
 ## Then create new SARIMA model via Statsmodels with selected parameters
-def create_best_model(timeseries_dataset,m=12,start_p=0,max_p=3
-                        ,start_q=0,max_q=3,d=n_d,start_P=0,
-                        start_Q=0, max_P=3, max_Q = 3, D = n_D):
+def create_best_model(timeseries_dataset,m=12,start_p=0,max_p=3,
+                        start_q=0,max_q=3,start_P=0,
+                        start_Q=0, max_P=3, max_Q = 3):
+
     """Calculates best model parameters via auto-arima,
      then fits a new SARIMAX model for results.
 
@@ -81,34 +107,89 @@ def create_best_model(timeseries_dataset,m=12,start_p=0,max_p=3
         max_p (int, optional): Max value for "p". Defaults to 3.
         start_q (int, optional): Starting value for "pq. Defaults to 0.
         max_q (int, optional): Max value for "q". Defaults to 3.
-        d ([type], optional): The order of first-differencing. Defaults to n_d.
         start_P (int, optional): Starting value for "P". Defaults to 0.
         start_Q (int, optional): Starting value for "Q". Defaults to 0.
         max_P (int, optional): Max value for "P". Defaults to 3.
         max_Q (int, optional): Max value for "Q". Defaults to 3.
-        D ([type], optional): The order of the seasonal differencing. Defaults to n_D.
 
     Returns:
         auto_model, best_model: auto_arima-generated model with best parameters,
                                 SARIMAX model using best parameters.
     """    
 
-    auto_model = auto_arima_model(timeseries_dataset, start_p = start_p,
-                                max_p = max_p,start_q = start_q,
-                                max_q = max_q, d = d ,m = m,
-                                start_P = start_P, start_Q = start_Q,
-                                max_P = max_P, max_Q = max_Q, D = D)
+    auto_model_best = auto_arima_model(timeseries_dataset,m = m,
+                                 start_p = start_p,max_p = max_p,
+                                 start_q = start_q,max_q = max_q,
+                                 start_P = start_P, start_Q = start_Q,
+                                 max_P = max_P, max_Q = max_Q)
       
-    best_model = tsa.SARIMAX(timeseries_dataset,order=auto_model.order,
-                             seasonal_order = auto_model.seasonal_order,
+    best_model = tsa.SARIMAX(timeseries_dataset,order=auto_model_best.order,
+                             seasonal_order = auto_model_best.seasonal_order,
                              enforce_invertibility=False).fit()
     
-    display(auto_model.summary())
+    display(auto_model_best.summary())
     display(model_results(best_model))
     
-    return auto_model, best_model
+    return auto_model_best, best_model
 
+## Using get_forecast to generate forecasted data
+def forecast_and_ci(model, test_data = None, n_yrs_future = None):
+    """Generate forecast for a given model
 
+    Args:
+        model: fitted SARIMAX model
+        test_data (Series): Test data
+    """    
+    if test_data is None:
+        forecast = model.get_forecast(steps=12*n_yrs_future)
+    else:
+        forecast = model.get_forecast(steps=len(test_data))
+    
+    forecast_df = forecast.conf_int()
+    forecast_df.columns = ['Lower CI','Upper CI']
+    forecast_df['Forecast'] = forecast.predicted_mean
+
+    return forecast_df
+
+## Plotting training, testing datasets
+def plot_forecast_ttf(train, test, forecast_df, n_yrs_past=5):
+    fig,ax = plt.subplots(figsize=(13,6))
+    last_n_lags=12*n_yrs_past
+    train.iloc[-last_n_lags:].plot(label='Training Data')
+    test.plot(label='Test Data')
+
+    ## Plotting forecasted data and confidence intervals
+    forecast_df['Forecast'].plot(ax=ax,label='Forecast')
+    ax.fill_between(forecast_df.index,forecast_df['Lower CI'],
+                    forecast_df['Upper CI'],color='b',alpha=0.4)
+    ax.set(xlabel='Time')
+    ax.set(ylabel='Sale Price ($)')
+    ax.set_title(f'Zipcode {train.name}: Training, Testing, and Forecasted Data')
+    ax.axvline(train.index[-1], linestyle=":", label='Beginning of Forecast')
+    ax.legend(loc='upper left')
+    plt.show()
+
+    return fig, ax
+
+## Plotting training, testing datasets
+def plot_forecast_final(zipcode_val, forecast_overall):
+    ## Plotting original data and forecasted results
+    fig,ax = plt.subplots(figsize=(13,6))
+
+    ## Plotting original data
+    zipcode_val.plot(label='Original Data')
+
+    ## Plotting forecasted data and confidence intervals
+    forecast_overall['Forecast'].plot(ax=ax,label='Forecast')
+    ax.fill_between(forecast_overall.index,forecast_overall['Lower CI'],
+                    forecast_overall['Upper CI'],color='b',alpha=0.4)
+    ax.set(xlabel='Time')
+    ax.set(ylabel='Sale Price ($)')
+    ax.set_title('Original and Forecasted Data')
+    ax.legend(loc='upper left')
+    plt.show()
+
+    return fig, ax
 
 
 ### --------------- Workflow --------------- ###
@@ -133,38 +214,15 @@ def ts_modeling_workflow(dataframe, zipcode, threshold = .85, m= 12, n_yrs_past=
     ## Split dataset
     train, test = ts_split(zipcode_val, threshold)
 
-    ## Determine d, D values for SARIMA model
-    n_d = ndiffs(train)
-    n_D = nsdiffs(train, m=m)
-
     ## Generating auto_arima model and SARIMAX model
     ## (based on best parameters from auto_arima model)
-    auto_model, best_model = create_best_model(train, n_d=n_d, n_D=n_D)
-  
-    ## Using get_forecast to generate forecasted data
-    forecast = best_model.get_forecast(steps=len(test))
+    auto_model, best_model = create_best_model(train)
+    
+    ## Generating dataframe to store forecast results
+    forecast_df = forecast_and_ci(best_model, test)
 
-    ## Saving confidence intervals and predicted mean for future
-    forecast_df = forecast.conf_int()
-    forecast_df.columns = ['Lower CI','Upper CI']
-    forecast_df['Forecast'] = forecast.predicted_mean
-    forecast_df.head(5)
-
-    fig,ax = plt.subplots(figsize=(13,6))
-
-    ## Plotting training, testing datasets
-    last_n_lags=12*n_yrs_past
-    train.iloc[-last_n_lags:].plot(label='Training Data')
-    test.plot(label='Test Data')
-
-    ## Plotting forecasted data and confidence intervals
-    forecast_df['Forecast'].plot(ax=ax,label='Forecast')
-    ax.fill_between(forecast_df.index,forecast_df['Lower CI'],
-                    forecast_df['Upper CI'],color='b',alpha=0.4)
-    ax.set(xlabel='Time')
-    ax.set(ylabel='Sale Price ($)')
-    ax.set_title('Original Data and Forecasted Data')
-    ax.legend();
+    ## Plotting forecast results against train/test split
+    plot_forecast_ttf(train=train, test=test, forecast_df = forecast_df)
 
     ## Fitting best model using whole dataset
     best_model_overall = tsa.SARIMAX(zipcode_val,order=auto_model.order,
@@ -175,28 +233,10 @@ def ts_modeling_workflow(dataframe, zipcode, threshold = .85, m= 12, n_yrs_past=
     display(model_results(best_model_overall))
 
     ## Using get_forecast to generate forecasted data
-    forecast = best_model_overall.get_forecast(steps=12*n_yrs_future)
+    forecast_overall = forecast_and_ci(best_model_overall, n_yrs_future = n_yrs_future)
 
-    ## Saving confidence intervals and predicted mean for future
-    forecast_df = forecast.conf_int()
-    forecast_df.columns = ['Lower CI','Upper CI']
-    forecast_df['Forecast'] = forecast.predicted_mean
-    display(forecast_df)
-
-    ## Plotting original data and forecasted results
-    fig,ax = plt.subplots(figsize=(13,6))
-
-    ## Plotting original data
-    zipcode_val.plot(label='Original Data')
-
-    ## Plotting forecasted data and confidence intervals
-    forecast_df['Forecast'].plot(ax=ax,label='Forecast')
-    ax.fill_between(forecast_df.index,forecast_df['Lower CI'],
-                    forecast_df['Upper CI'],color='b',alpha=0.4)
-    ax.set(xlabel='Time')
-    ax.set(ylabel='Sale Price ($)')
-    ax.set_title('Original and Forecasted Data')
-    ax.legend();
+    ## Plotting original data and forecast results
+    plot_forecast_final(zipcode_val, forecast_overall)
 
     ## Calculating investment cost and ROI
     investment_cost = forecast_df.iloc[0,2]
